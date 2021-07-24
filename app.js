91e887759328e1f728299021e7883b9c5a6f4503
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2021-07-24 17:14:20 +0530

    now bots find one week vaccine availability

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const Discord = require('discord.js')
 const fetch = require('node-fetch')
 const bot = new Discord.Client()
 const TOKEN = process.env.TOKEN
-const DELAY = 60* 1000 //fetch in every 60 seconds
-const DELAY_DATE = 6
+const DELAY = 2 * 60 * 1000 //fetch in every 2 minutes
+const DELAY_DATE = 7
 const SETU_URL = process.env.SETU_URL
 const CHANNEL = process.env.CHANNEL
-let lastMsg = ''
+let lastMsg = ['','','','','','']
 bot.login(TOKEN)
 
 bot.on('ready', () => {
@@ -15,23 +15,25 @@ bot.on('ready', () => {
 })
 
 const fetchData = async()=>{
-    const tmrw = new Date(new Date().getTime() + DELAY_DATE * 24 * 60 * 60 * 1000)
-    const day = tmrw.getDate(), month = tmrw.getMonth()+1 , year = tmrw.getFullYear()
-    const strd = `${day<10?('0'+day):day}-${month<10?('0'+month):month}-${year}`
-    const res = await fetch(SETU_URL+strd)
-    if(res.ok){
-        const data =await res.json()
-        const sessions =await data.sessions
-        let msg = ''
-        sessions.forEach(s => {
-            Object.keys(s).forEach(k=>{
-                msg+=`${k}:${s[k]}\n`
-            })
-            msg+='-------------------\n'
-        });
-        if(lastMsg !== msg)
-            bot.channels.cache.get(CHANNEL).send(msg)
-        lastMsg = msg
+    for(let i=1;i<=DELAY_DATE;++i){
+        const tmrw = new Date(new Date().getTime() + i * 24 * 60 * 60 * 1000)
+        const day = tmrw.getDate(), month = tmrw.getMonth()+1 , year = tmrw.getFullYear()
+        const strd = `${day<10?('0'+day):day}-${month<10?('0'+month):month}-${year}`
+        const res = await fetch(SETU_URL+strd)
+        if(res.ok){
+            const data =await res.json()
+            const sessions =await data.sessions
+            let msg = ''
+            sessions.forEach(s => {
+                Object.keys(s).forEach(k=>{
+                    msg+=`${k}:${s[k]}\n`
+                })
+                msg+='-------------------\n'
+            });
+            if(lastMsg[i] !== msg && msg)
+                bot.channels.cache.get(CHANNEL).send(msg)
+            lastMsg[i] = msg
+        }
     }
 }
 

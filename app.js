27c915a689505a205705b89fabdbc9da80c96742
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2022-01-04 22:40:01 +0530

    version updated and now it is echo bot

--- app.js
+++ app.js
@@ -1,49 +1,21 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const fetch = require('node-fetch')
-const bot = new Discord.Client()
-const TOKEN = process.env.TOKEN
-const DELAY = 2 * 60 * 1000 //fetch in every 2 minutes
-const DELAY_DATE = 7
-const SETU_URL = process.env.SETU_URL
-const CHANNEL = process.env.CHANNEL
-const SEPERATOR = '\0'
-const UNNECCESSARY_KEYS = ['center_id','pincode','from','to','lat','long','fee','session_id','fee_type','alow_all_age','slots','state_name']
-const LINE_SEPERATOR = '\n-------------------\n'
-let lastMsg = ['','','','','','']
-bot.login(TOKEN)
+require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`)
-})
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-const fetchData = async()=>{
-    for(let i=1;i<=DELAY_DATE;++i){
-        const tmrw = new Date(new Date().getTime() + i * 24 * 60 * 60 * 1000)
-        const day = tmrw.getDate(), month = tmrw.getMonth()+1 , year = tmrw.getFullYear()
-        const strd = `${day<10?('0'+day):day}-${month<10?('0'+month):month}-${year}`
-        const res = await fetch(SETU_URL+strd)
-        if(res.ok){
-            const data =await res.json()
-            const sessions =await data.sessions
-            let msg = ''
-            sessions.sort((a,b)=>a['address']>b['address']).forEach(s => {
-                Object.keys(s).filter(e=>!(UNNECCESSARY_KEYS.includes(e))).forEach(k=>{
-                    if(s['available_capacity'])
-                        msg+=`${k}:${s[k]}\n`
-                })
-                msg+=SEPERATOR
-            })
-            if(lastMsg[i] !== msg && msg){
-                msg.split(SEPERATOR).forEach(async(m)=>{
-                    if (m) {
-                        await bot.channels.cache.get(CHANNEL).send(m+LINE_SEPERATOR)
-                    }
-                })
-            }
-            lastMsg[i] = msg
-        }
-    }
-}
+client.on('messageCreate', (message) => {
+  if (message.content.startsWith('..')) {
+    message
+      .reply(message.content.replace('..', ''))
+      .then((msg) => {
+        message.delete();
+      })
+      .catch((err) => {});
+  }
+});
 
-setInterval(fetchData, DELAY)
+client.login(TOKEN);
